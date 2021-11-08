235a008a1c0b40eeeef8368c498ce01a441cb769
Brad Murphy <bradmurphy@google.com>
2021-11-08 14:19:54 -0500

    feat: refactoring main plugin, setting up test case

--- src/sitemap.test.ts
+++ src/sitemap.test.ts
@@ -1,18 +1,11 @@
-// import {ExecutionContext} from 'ava';
-// import {GreenhousePlugin} from './greenhouse';
-// import {Pod} from '@amagaki/amagaki';
-// import test from 'ava';
+import {ExecutionContext} from 'ava';
+import {SitemapPlugin} from './sitemap';
+import {Pod} from '@amagaki/amagaki';
+import test from 'ava';
 
-// test('Test GreenhousePlugin', async (t: ExecutionContext) => {
-//   const pod = new Pod('../example');
-//   const greenhouse = GreenhousePlugin.register(pod, {
-//     boardToken: 'vaulttec',
-//   });
-//   await greenhouse.bindCollection({
-//     collectionPath: '/content/jobs',
-//   });
-//   await greenhouse.saveEducationFile({
-//     podPath: '/content/partials/education.yaml',
-//   });
-//   t.pass();
-// });
+test('Test SitemapPlugin', async (t: ExecutionContext) => {
+  const pod = new Pod('../example');
+  const sitemap = SitemapPlugin.register(pod);
+  console.log(sitemap);
+  t.pass();
+});
