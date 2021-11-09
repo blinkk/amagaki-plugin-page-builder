046cf6b5e9f7af083fe2bef35e17e84dd55c7c60
Brad Murphy <bradmurphy@google.com>
2021-11-09 16:17:05 -0500

    Updating Test Name and building out example folder

--- example/amagaki.ts
+++ example/amagaki.ts
@@ -1,19 +1,12 @@
-// import {BuilderPlugin, Pod} from '@amagaki/amagaki';
+import {BuilderPlugin, Pod} from '@amagaki/amagaki';
 
-// // eslint-disable-next-line node/no-unpublished-import
-// import {GreenhousePlugin} from '../dist';
+// eslint-disable-next-line node/no-unpublished-import
+import {SitemapPlugin} from '../dist';
 
-// export default async (pod: Pod) => {
-//   const builderPlugin = pod.plugins.get('BuilderPlugin') as BuilderPlugin;
-//   builderPlugin.addBeforeBuildStep(async () => {
-//     const greenhouse = GreenhousePlugin.register(pod, {
-//       boardToken: 'vaulttec',
-//     });
-//     await greenhouse.bindCollection({
-//       collectionPath: '/content/jobs',
-//     });
-//     await greenhouse.saveEducationFile({
-//       podPath: '/content/partials/education.yaml',
-//     });
-//   });
-// };
+export default async (pod: Pod) => {
+  const builderPlugin = pod.plugins.get('BuilderPlugin') as BuilderPlugin;
+  builderPlugin.addBeforeBuildStep(async () => {
+    const sitemap = SitemapPlugin.register(pod);
+    console.log(sitemap)
+  });
+};
--- src/sitemap.test.ts
+++ src/sitemap.test.ts
@@ -3,7 +3,7 @@ import {SitemapPlugin} from './sitemap';
 import {Pod} from '@amagaki/amagaki';
 import test from 'ava';
 
-test('Test SitemapPlugin', async (t: ExecutionContext) => {
+test('SitemapPlugin', async (t: ExecutionContext) => {
   const pod = new Pod('../example');
   const sitemap = SitemapPlugin.register(pod);
   console.log(sitemap);
