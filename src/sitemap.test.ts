c6420113d0f31d9c8fcdab46f67ca7296abe4e8b
Brad Murphy <bradmurphy@google.com>
2021-11-15 13:50:43 -0500

    Making adjustments for sitemap and robots text path options

--- src/sitemap.test.ts
+++ src/sitemap.test.ts
@@ -5,7 +5,7 @@ import test from 'ava';
 test('SitemapPlugin: sitemap.xml', async (t: ExecutionContext) => {
   const pod = new Pod('./example');
   await pod.router.warmup();
-  const sitemapRoute = await pod.router.resolve('/sitemap.xml') as Route;
+  const sitemapRoute = await pod.router.resolve('/foo/sitemap.xml') as Route;
   const sitemapContent = await sitemapRoute.build();
   const sitemapExpected = `
   <?xml version="1.0" encoding="UTF-8"?>
@@ -29,12 +29,11 @@ test('SitemapPlugin: sitemap.xml', async (t: ExecutionContext) => {
 test('SitemapPlugin: robots.txt', async (t: ExecutionContext) => {
   const pod = new Pod('./example');
   await pod.router.warmup();
-  const robotsRoute = await pod.router.resolve('/robots.txt') as Route;
+  const robotsRoute = await pod.router.resolve('/bar/robots.txt') as Route;
   const robotsContent = await robotsRoute.build();
   const robotsExpected = `
       User-agent: *
       Allow: /
-      Sitemap: doc.sitemap
     `;
   t.deepEqual(robotsContent, robotsExpected);
 });
